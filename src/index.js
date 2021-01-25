a822681b4aee49e765c2db81c3e13209b4ad7208
Quincy <kuncc@foxmail.com>
2021-01-25 23:41:25 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-console.log('xx')
+import x from './x.js'
+console.log(x)
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,25 @@
+let HtmlWebpackPlugin = require('html-webpack-plugin')
+let path = require('path')
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
   output: {
     filename: "index.[contenthash].js"
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: "Development",
+    template: "src/assets/index.html"
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 }
