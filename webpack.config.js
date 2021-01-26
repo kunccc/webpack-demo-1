c085c3e78968a1dfbbd570746988eab20526e71e
Quincy <kuncc@foxmail.com>
2021-01-26 11:51:56 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path')
+const base = require('./webpack.config.base')
 module.exports = {
+  ...base,
   mode: "development",
-  entry: "./src/index.js",
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist"
   },
-  output: {
-    filename: "index.[contenthash].js"
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Development",
-      template: "src/assets/index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-      chunkFilename: '[id].[contenthash].css',
-    })
-  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: '/public/path/to/',
-            },
-          },
-          'css-loader',
-        ],
-        // use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader"],
       },
     ],
-  }
-  ,
+  },
 }
